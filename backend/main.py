f73d2f3c4461d714201eefa177c585218f19d111
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2025-06-19 18:01:57 +0500

    Fix admin static files issue

--- backend/main.py
+++ backend/main.py
@@ -15,4 +15,4 @@ app.include_router(api_router_v1, prefix="/api/v1")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=80, forwarded_allow_ips="*", proxy_headers=True)
