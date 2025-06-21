d930dc5215326d2afcee780452416ee57241ac03
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2025-06-21 18:54:40 +0500

    Use bigint for storing telegram ids

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,12 +1,13 @@
 from models.base import Base, created_at, intpk, updated_at
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import BIGINT
 
 
 class User(Base):
     __tablename__ = "user_account"
 
     id: Mapped[intpk]
-    telegram_id: Mapped[int] = mapped_column(unique=True, index=True)
+    telegram_id: Mapped[int] = mapped_column(BIGINT, unique=True, index=True)
     name: Mapped[str]
     is_active: Mapped[bool] = mapped_column(default=False, index=True)
 
